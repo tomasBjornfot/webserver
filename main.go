3e9ee1a37acbfbcd389734b71866cefdba1dbc15
Tomas Bjornfot <tbjornfot@cochlear.com>
2018-11-19 15:45:02 +0100

    new commit

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"log"
 	"encoding/json"
-	
+	"fmt"
 )
 /*
  * STRUCTS 
@@ -65,6 +65,31 @@ func read_settings(dir string) *Settings {
 /*
  * FUNCTIONS AND METHODS
  */
+ func calc_cs(mesh *stl.Mesh, max_distance float64) {
+	y_cs := []float64
+	index := 0
+	md2 := max_distance*max_distance
+	p0 := mesh.Profile[0]
+	for (p0[1] < mesh.Profile[len(mesh.Profile)-1][1])){
+		// set next point as line edge
+		p1 := mesh.Profile[index + 1] 
+		// calc if line edge is too far away
+		dx := sp[0]-np[0]
+		dy := sp[1]-np[1]
+		if (dx*dx+dy*dy < md2) {
+			// ok, use it
+			p0 = p1
+			y_cs = append(y_cs, p0[1])
+			index ++
+			continue
+		}
+		// nok, take the half distance to edge of line
+		p0[1] = p0[1] + (p1[1] - p0[1])/2.0
+		k := dy/dx
+		m := sp[1] - k*sp[0]
+		p0[0] = (y - k)/m
+	}
+}
 
 /*
  * COMPOSITE FUNCTIONS
@@ -79,9 +104,6 @@ func prepare_stl(path string) (*stl.Mesh, *stl.Mesh) {
 	bottom.Rotate("y", 180.0)
 	return deck, bottom
 }
-func calc_cs(*stl.Mesh) {
-	
-}
 /*
  * MAIN FUNCTION
  */
@@ -91,7 +113,9 @@ func main() {
 	// prepare the STL file
 	deck, bottom := prepare_stl("c:\\tmp\\test.stl")
 	// calculates the deck
-	cs_deck := calc_cs(deck)
-	// calculates the botom
-	cs_bottom := calc_cs(bottom)
+	calc_cs(deck, 10.0)
+	// 
+	fmt.Println(settings.ToolRadius)
+	fmt.Println(deck.No_tri)
+	fmt.Println(bottom.No_tri)
 }
